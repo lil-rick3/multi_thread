c1b66047bfe4a24d2eae35a84b09258388c0a002
lil-rick3 <83485898+lil-rick3@users.noreply.github.com>
2024-11-04 16:10:51 -0700

    Update another_test.c

--- another_test.c
+++ another_test.c
@@ -1,1 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+
+// Function to be executed by each thread
+void* threadFunction(void* arg) {
+    int thread_id = *(int*)arg;
+    printf("Hello from thread %d\n", thread_id);
+    sleep(1); // Simulate some work being done
+    return NULL;
+}
+
+int main() {
+    int num_threads = 5;
+    pthread_t threads[num_threads];
+    int thread_ids[num_threads];
+
+    // Create threads
+    for (int i = 0; i < num_threads; i++) {
+        thread_ids[i] = i + 1;
+        if (pthread_create(&threads[i], NULL, threadFunction, &thread_ids[i]) != 0) {
+            perror("Failed to create thread");
+            return 1;
+        }
+    }
+
+    // Join threads
+    for (int i = 0; i < num_threads; i++) {
+        if (pthread_join(threads[i], NULL) != 0) {
+            perror("Failed to join thread");
+            return 1;
+        }
+    }
+
+    printf("All threads have finished execution.\n");
+    return 0;
+}
 
